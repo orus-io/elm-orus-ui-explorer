4e6479eda7947be97247187286f68fe7e845da7b
Christophe de Vienne <christophe.devienne@orus.io>
2024-11-22 21:38:55 +0100

    Keycolors: Add a edit icon on mouseOver

---
+++ src/OUI/Showcase/IconsCat.elm
@@ -0,0 +1,11 @@
+module OUI.Showcase.IconsCat exposing (..)
+
+import Material.Icons.Outlined
+import Material.Icons.Types exposing (Coloring(..))
+import OUI.Icon exposing (Icon, elmMaterialIcons)
+
+
+edit : Icon
+edit =
+    Material.Icons.Outlined.edit
+        |> elmMaterialIcons Color
